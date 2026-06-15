00d239fbb30d0c27d1d377a07df4413cc8701b72
Ana Barbosa <ana.barbosa.dev@gmail.com>
2026-06-14 22:44:20 -0300

    style: formata index de Autocadastro

--- src/features/Autocadastro/index.tsx
+++ src/features/Autocadastro/index.tsx
@@ -1,10 +1,10 @@
-import RegisterForm from './forms/register'
+import RegisterForm from "./forms/register";
 
 export default function AutocadastroPage() {
-  return (
-    <div className="d-flex flex-col items-center justify-center min-h-screen">
-      <h1>AUTOCADASTRO</h1>
-      <RegisterForm />
-    </div>
-  )
+	return (
+		<div className="d-flex flex-col items-center justify-center min-h-screen">
+			<h1>AUTOCADASTRO</h1>
+			<RegisterForm />
+		</div>
+	);
 }
