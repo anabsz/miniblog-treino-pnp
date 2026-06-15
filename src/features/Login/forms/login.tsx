23bc93a552af815fc50207d93d312f69cbf5a36d
Ana Barbosa <ana.barbosa.dev@gmail.com>
2026-06-15 15:35:56 -0300

    feat(Login): adicionar schema e validação com yup e hookform(#3)

--- src/features/Login/forms/login.tsx
+++ src/features/Login/forms/login.tsx
@@ -1,25 +1,72 @@
 import { BrButton, BrInput } from "@govbr-ds/react-components";
 import { useState } from "react";
+import type { iLoginForm } from "../interfaces/iLoginForm";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { schema } from "./schema";
 
 export default function LoginForm() {
+	const [loading, setLoading] = useState(false);
 	const [showPassword, setShowPassword] = useState(false);
 
 	function togglePassword() {
 		setShowPassword((prev) => !prev);
 	}
 
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<iLoginForm>({
+		resolver: yupResolver(schema),
+	});
+
+	async function handleData(data: iLoginForm) {
+		setLoading(true);
+
+		const delay = (ms: number) =>
+			new Promise((resolve) => setTimeout(resolve, ms));
+		await delay(2000); // Simula um atraso de 2 segundos
+
+		try {
+			console.log("Dados do formulário:", data);
+		} catch (error) {
+			console.error("Erro ao processar o login:", error);
+		} finally {
+			setLoading(false);
+		}
+	}
+
 	return (
-		<form>
-			<BrInput label="Nome de Usuário" />
+		<form onSubmit={handleSubmit(handleData)}>
+			<BrInput
+				label="Nome de Usuário"
+				{...register("username")}
+				status={errors.username ? "danger" : undefined}
+				feedbackText={errors.username?.message}
+			/>
 			<BrInput
 				label="Senha"
 				type={showPassword ? "text" : "password"}
+				{...register("password")}
+				status={errors.password ? "danger" : undefined}
+				feedbackText={errors.password?.message}
 			>
 				<BrButton
 					icon={showPassword ? "fa-solid fa-eye-slash" : "fa-solid fa-eye"}
 					onClick={togglePassword}
 				/>
 			</BrInput>
+			<div>
+				<BrButton
+					primary
+					type="submit"
+					className="w-full! mx-auto"
+					disabled={loading}
+				>
+					Entrar
+				</BrButton>
+			</div>
 		</form>
 	);
 }
