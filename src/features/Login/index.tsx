ec457ac020f272874b37093bf4a248d1d798f108
Ana Barbosa <ana.barbosa.dev@gmail.com>
2026-06-16 10:48:25 -0300

    feat(Login): adicionar service de login(#3)

--- src/features/Login/index.tsx
+++ src/features/Login/index.tsx
@@ -1,5 +1,12 @@
 import LoginForm from "./forms/login";
 
 export default function LoginPage() {
-	return <LoginForm />;
+	return (
+		<div className="container d-flex flex-col items-center justify-center min-h-screen">
+			<div className="w-lg d-flex flex-col gap-2 px-4 py-8 bg-pure-0 shadow-xl">
+				<h1>Login</h1>
+				<LoginForm />
+			</div>
+		</div>
+	);
 }
