3bb73fca621e9c75f8ca61e82a9a9bcbf5bf52cb
Ana Barbosa <ana.barbosa.dev@gmail.com>
2026-06-22 15:31:30 -0300

    feat(routes): adicionar rota de publicacao(#9)

--- src/routes/protected.tsx
+++ src/routes/protected.tsx
@@ -1,9 +1,19 @@
+import DetalharPublicacao from "../features/DetalharPublicacao";
 import HomePage from "../pages/Home";
 import Layout from "../pages/layout";
 
 export const protectedRoutes = [
 	{
 		element: <Layout />,
-		children: [{ path: "/", element: <HomePage /> }],
+		children: [
+			{
+				path: "/",
+				element: <HomePage />,
+			},
+			{
+				path: "/publicacao/:id",
+				element: <DetalharPublicacao />,
+			},
+		],
 	},
 ];
